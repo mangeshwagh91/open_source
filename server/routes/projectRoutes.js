2315bfaf79ed5c33a302b5f210ca5074fffe3dc6
mangeshwagh91 <wmangesh91@gmail.com>
2025-12-24 16:00:35 +0530

    feat: allow mentors to create, update, and delete projects and users

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -14,11 +14,11 @@ const router = express.Router();
 
 router.route('/')
   .get(validate(querySchema), getProjects)
-  .post(protect, authorize('mentor','admin'), validate(projectSchema), createProject);
+  .post(protect, authorize('admin', 'mentor'), validate(projectSchema), createProject);
 
 router.route('/:id')
   .get(validate(projectSchema), getProjectById)
-  .put(protect, authorize('mentor','admin'), validate(projectSchema), updateProject)
-  .delete(protect, authorize('mentor','admin'), validate(projectSchema), deleteProject);
+  .put(protect, authorize('admin', 'mentor'), validate(projectSchema), updateProject)
+  .delete(protect, authorize('admin', 'mentor'), validate(projectSchema), deleteProject);
 
 export default router;
